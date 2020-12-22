4000b5dc2bc7525a7f96c828c9b454ac0ed3a277
Josh K Preston <kanemetal@gmail.com>
2020-12-22 14:48:38 -0700

    save

--- amazen_server/Program.cs
+++ amazen_server/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace amazen.server
+namespace amazen_server
 {
     public class Program
     {
